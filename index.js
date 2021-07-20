f24d0892b1f542c80752e603b98e892f1f3561bd
John Hiesey <john@hiesey.com>
2021-07-19 20:15:38 -0700

    Hacky attempt to treat unparseable paths as top level

--- index.js
+++ index.js
@@ -2,10 +2,17 @@
 
 var path = require('path')
 
-module.exports = function (file) {
+module.exports = function (file, topLevelPath) {
   var segments = file.split(path.sep)
   var index = segments.lastIndexOf('node_modules')
-  if (index === -1) return
+  if (index === -1 && typeof topLevelPath === 'string') {
+    // HACK: this approach may not be a good idea...
+    topLevelPath = path.normalize(topLevelPath)
+    if (file.startsWith(topLevelPath + path.sep)) {
+      index = topLevelPath.split(path.sep).length - 2
+    }
+  }
+  if (index < 0) return
   if (!segments[index + 1]) return
   var scoped = segments[index + 1][0] === '@'
   var name = scoped ? segments[index + 1] + '/' + segments[index + 2] : segments[index + 1]
